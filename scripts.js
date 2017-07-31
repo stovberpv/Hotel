d6cb9584a575c44918ae22032b72ab928bc6adc6
Pavel <stovberpv@yandex.ru>
2017-07-31 23:44:02 +0300

    some changes

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
+﻿var selected = new Array();
+
 //
 window.onload = function onload() {
     "use strict";
     createSelectMonthSelect();
     setYear();
     createCalendarTable();
-    addRooms();
     
     document.getElementById('selectMonth').addEventListener('change', createCalendarTable);
 }
-
-
 
 //
 function createSelectMonthSelect() {
@@ -48,31 +47,47 @@ function setYear(){
 function createCalendarTable() {
     delRowDays();
     setRowDays();
+    addRooms();
 }
 
 //
 function delRowDays() {
     "use strict";
-    var tbody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
+    var calendar = document.getElementById('calendar');
+
+    var thead = calendar.getElementsByTagName('thead')[0];
+    var tr = thead.getElementsByTagName('tr');
+    for (var i = tr.length; i > 0; i--) {
+	thead.removeChild(tr[0]);
+    }
+    
+    var tbody = calendar.getElementsByTagName('tbody')[0];
     var tr = tbody.getElementsByTagName('tr');
-//    tbody.removeAttribute(tr);
-    tbody.removeChild;
+    for (var i = tr.length; i > 0; i--) {
+	tbody.removeChild(tr[0]);
+    }
 }
+
 //
 function setRowDays(month) {
     "use strict";
-    var month = document.getElementById('selectMonth').value;
-    var year = document.getElementById('selectYear').value;
-    var days = new Date(year, overlay(month, 0, 2), 0).getDate();
+    var month = document.getElementById('selectMonth').value,
+    	year = document.getElementById('selectYear').value,
+    	days = new Date(year, overlay(month, 0, 2), 0).getDate();
     
-    var tbody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
-    var daysRow = tbody.insertRow(tbody.rows.length);
+    var thead = document.getElementById('calendar').getElementsByTagName('thead')[0],
+	tr = document.createElement('tr');
     
     for (var i = 0; i <= days; i++) {
-        var dayCell = daysRow.insertCell(i)
-        var textCell = document.createTextNode(i);
-        dayCell.appendChild(textCell);
+        var td = document.createElement('th');
+	if (i == 0) {
+		td.appendChild(document.createTextNode("R"));
+	} else {
+		td.appendChild(document.createTextNode(i));
+	}
+        tr.appendChild(td);
     }
+    thead.appendChild(tr);
 }
 
 // 
@@ -88,13 +103,53 @@ function addRooms(){
     "use strict";
     var rooms = [11, 12, 13, 21, 22, 23, 31, 32, 32, 34];
     
+    var totalCells = document
+			.getElementById('calendar')
+			.getElementsByTagName('thead')[0]
+			.getElementsByTagName('tr')[0]
+			.childElementCount;
+
     var tbody = document.getElementById('calendar').getElementsByTagName('tbody')[0];
-    var tr = tbody.rows[0];
-    tr.cells[0].textContent = " ";
-    
     for (var i = 0; i < rooms.length; i++) {
-        var newRow = tr.cloneNode(true);
-        var cell = newRow.cells[0].textContent = rooms[i];
-        tbody.appendChild(newRow);
+	var tr = document.createElement('tr');
+    	for (var j = 0; j < totalCells; j++) {
+		if (j == 0) {
+			var node = document.createElement('th');
+			node.appendChild(document.createTextNode(rooms[i]));
+		} else {
+			var node = document.createElement('td');
+			node.id = rooms[i];
+			node.addEventListener('click', cellClick);
+			node.appendChild(document.createTextNode(""));
+		}
+	tr.appendChild(node);
+	}
+	tbody.appendChild(tr);
     }
+}
+
+//
+function cellClick() {
+	var room = parseInt(this.id);
+	var day = this.cellIndex;
+	
+	var res = isSelected([room, day]);
+	
+	if (res[0]) {
+        selected.splice(res[1], 1);
+        this.removeAttribute('class');
+	} else {
+		selected.push([room, day]);
+        this.setAttribute('class', 'selected');
+	}
+}
+
+//
+function isSelected(item) {
+    for (var i = 0; i < selected.length; i++) {
+        if (selected[i][0] == item[0] && selected[i][1] == item[1]) {
+            return [true, i];
+        }
+    }
+    return [false, -1];
 }
